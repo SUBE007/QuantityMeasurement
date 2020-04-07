b47376ec9b2bb2d8abfe709062faa4fa0fef3bda
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-07 12:00:49 +0530

    i want to equate the tempretur in Fahrenheit and Celcius

--- src/main/java/quantitymeasure/Tempreture.java
+++ src/main/java/quantitymeasure/Tempreture.java
@@ -1,17 +1,15 @@
 package quantitymeasure;
 
-public class Weight {
+public class Tempreture {
     public enum UnitConversionType {
         FEET(1 * 12.0),
         INCH(1.0),
         YARD(1 * 36.0), CENTIMETER(1 / 2.5),
-        GALLON(1 * 3.78),
-        LITRE(1.0),
-        MILLILITRE(1 / 1000.0),
-        TONNE(1*1000.0), KILOGRAMS(1.0), GRAMS(1/1000.0);
-
-
+        GALLON(1 * 3.78), LITRE(1.0),MILLILITRE(1 / 1000.0),
+        TONNE(1*1000.0), KILOGRAMS(1.0), GRAMS(1/1000.0),
+        FAHRENHEIT(1.0),CELSIUS(9/5.0);
         public Double type;
+
         UnitConversionType(Double type){
             this.type=type;
         }
@@ -21,13 +19,15 @@ public class Weight {
         }
     }
     public double unitConvertor(UnitConversionType unit, double value) {
-        return value*unit.getType();
+        if(unit == UnitConversionType.CELSIUS)
+            return value * unit.getType() + 32;
+         return value * unit.getType();
     }
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return true;
-        Weight feet = (Weight) obj;
+        Tempreture feet = (Tempreture) obj;
         return false;
     }
 }
--- src/test/java/quantitymeasure/QuantityTest.java
+++ src/test/java/quantitymeasure/QuantityTest.java
@@ -5,96 +5,61 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class QuantityTest {
-    Weight weight;
+     Tempreture tempreture;
     //double inch,inch1,inch2,feet1,feet2,feet,yard,cm1,cm2;
     //double volume1,volume2,gallon,litre,millilitre;
-    double weight2,kilograms,grams,tonne,weight1;
+    //double weight2,kilograms,grams,tonne,weight1;
+    double temprature1,temprature2,fahrenheit,celsius;
     @Before
     public void setUp() throws Exception {
-        weight = new Weight();
-    }
-
-//Weight compare
-@Test
-public void given0KilogramAnd0Kilograms_ShouldReturnEqual() {
-    weight1 = weight.unitConvertor(Weight.UnitConversionType.KILOGRAMS, 0);
-    weight2 = weight.unitConvertor(Weight.UnitConversionType.KILOGRAMS, 0);
-    Assert.assertEquals(weight1,weight2,0.0);
-}
-
-    @Test
-    public void given0KilogramAnd1Kilograms_ShouldReturnNotEqual() {
-        weight1 = weight.unitConvertor(Weight.UnitConversionType.KILOGRAMS, 0);
-        weight2 = weight.unitConvertor(Weight.UnitConversionType.KILOGRAMS, 1);
-        Assert.assertNotEquals(weight1,weight2,0.0);
+        tempreture = new Tempreture();
     }
 
     @Test
-    public void givenKilogramNull_WhenNull_ShouldReturnTrue() {
-        boolean nullCheck = weight.equals(null);
+    public void given0FahrenheitAnd0Fahrenheit_ShouldReturnEqual() {
+        temprature1 = tempreture.unitConvertor(Tempreture.UnitConversionType.FAHRENHEIT, 0);
+        temprature2 = tempreture.unitConvertor(Tempreture.UnitConversionType.FAHRENHEIT, 0);
+        Assert.assertEquals(temprature1,temprature2,0.0);
+    }
+
+    @Test
+    public void given0FahrenheitAnd1Fahrenheit_ShouldReturnNotEqual() {
+        temprature1 = tempreture.unitConvertor(Tempreture.UnitConversionType.FAHRENHEIT, 0);
+        temprature2 = tempreture.unitConvertor(Tempreture.UnitConversionType.FAHRENHEIT, 1);
+        Assert.assertNotEquals(temprature1,temprature2,0.0);
+    }
+
+    @Test
+    public void givenFahrenheitNull_WhenNull_ShouldReturnTrue() {
+        boolean nullCheck = tempreture.equals(null);
         Assert.assertTrue(nullCheck);
     }
 
     @Test
-    public void given0GramAnd0Grams_ShouldReturnEqual() {
-        weight1 = weight.unitConvertor(Weight.UnitConversionType.GRAMS, 0);
-        weight2 = weight.unitConvertor(Weight.UnitConversionType.GRAMS, 0);
-        Assert.assertEquals(weight1,weight2,0.0);
+    public void given0CelsiusAnd0Celsius_ShouldReturnEqual() {
+        temprature1 = tempreture.unitConvertor(Tempreture.UnitConversionType.CELSIUS, 0);
+        temprature2 = tempreture.unitConvertor(Tempreture.UnitConversionType.CELSIUS, 0);
+        Assert.assertEquals(temprature1,temprature2,0.0);
     }
 
     @Test
-    public void given0GramAnd1Grams_ShouldReturnEqual() {
-        weight1 = weight.unitConvertor(Weight.UnitConversionType.GRAMS, 0);
-        weight2 = weight.unitConvertor(Weight.UnitConversionType.GRAMS, 1);
-        Assert.assertNotEquals(weight1,weight2,0.0);
+    public void given0CelsiusAnd1Celsius_ShouldReturnEqual() {
+        temprature1 = tempreture.unitConvertor(Tempreture.UnitConversionType.CELSIUS, 0);
+        temprature2 = tempreture.unitConvertor(Tempreture.UnitConversionType.CELSIUS, 1);
+        Assert.assertNotEquals(temprature1,temprature2,0.0);
     }
 
     @Test
-    public void givenGramNull_WhenNull_ShouldReturnTrue() {
-        boolean nullCheck = weight.equals(null);
+    public void givenCelsiusNull_WhenNull_ShouldReturnTrue() {
+        boolean nullCheck = tempreture.equals(null);
         Assert.assertTrue(nullCheck);
     }
 
     @Test
-    public void given0TonneAnd0Tonne_ShouldReturnEqual() {
-        weight1 = weight.unitConvertor(Weight.UnitConversionType.TONNE, 0);
-        weight2 = weight.unitConvertor(Weight.UnitConversionType.TONNE, 0);
-        Assert.assertEquals(weight1,weight2,0.0);
-    }
-
-    @Test
-    public void given0TonneAnd1Tonne_ShouldReturnNotEqual() {
-        weight1 = weight.unitConvertor(Weight.UnitConversionType.TONNE, 0);
-        weight2 = weight.unitConvertor(Weight.UnitConversionType.TONNE, 1);
-        Assert.assertNotEquals(weight1,weight2,0.0);
-    }
-
-    @Test
-    public void givenTonneNull_WhenNull_ShouldReturnTrue() {
-        boolean nullCheck = weight.equals(null);
-        Assert.assertTrue(nullCheck);
-    }
-
-    @Test
-    public void given1KilogramAnd1000Grams_ShouldReturnEqual() {
-        kilograms = weight.unitConvertor(Weight.UnitConversionType.KILOGRAMS, 1);
-        grams = weight.unitConvertor(Weight.UnitConversionType.GRAMS, 1000);
-        Assert.assertEquals(grams,kilograms,0.0);
-    }
-
-    @Test
-    public void given1TonneAnd1000Kilograms_ShouldReturnEqual() {
-        tonne = weight.unitConvertor(Weight.UnitConversionType.TONNE, 1);
-        kilograms = weight.unitConvertor(Weight.UnitConversionType.KILOGRAMS, 1000.0);
-        Assert.assertEquals(tonne,kilograms,0.0);
-    }
-//Addition
-    @Test
-    public void given1TonneAnd1000Grams_ShouldReturnEqualToThousandAndOneKilograms() {
-        tonne = weight.unitConvertor(Weight.UnitConversionType.TONNE, 1);
-        grams = weight.unitConvertor(Weight.UnitConversionType.GRAMS, 1000.0);
-        double kilograms = tonne + grams;
-        Assert.assertEquals(1001, kilograms, 0.0);
+    public void givenTemperatureInFahrenheit_ShouldReturnTemperatureInCelsius() {
+        fahrenheit = tempreture.unitConvertor(Tempreture.UnitConversionType.FAHRENHEIT, 212);
+        celsius = tempreture.unitConvertor(Tempreture.UnitConversionType.CELSIUS,100);
+        Assert.assertEquals(celsius,fahrenheit,0.0);
     }
 
 }
